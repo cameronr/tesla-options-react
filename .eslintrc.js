a8e19811775b096073a196f55050232ecdb920aa
Cameron Ring <cameron@cs.stanford.edu>
2017-09-11 01:36:24 +0000

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "airbnb",
+  "parser": "babel-eslint",
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "jsx-a11y/href-no-hash": "off"
+  },
+  "env": {
+    "browser": true,
+    "node": true,
+  },
+};

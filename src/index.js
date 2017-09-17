82dbea3dd007d082b4e047779501e55545ff4ee6
Cameron Ring <cameron@cs.stanford.edu>
2017-09-17 06:29:02 +0000

    remove service worker

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router } from 'react-router-dom'
-import appStore from './reducers'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { unregister } from './registerServiceWorker';
+import appStore from './reducers';
 import App from './App';
 import './index.css';
 
-let store = createStore(appStore)
+const store = createStore(appStore);
 
 render(
   <Provider store={store}>
@@ -16,6 +16,7 @@ render(
       <App />
     </Router>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-registerServiceWorker();
+// registerServiceWorker();
+unregister();

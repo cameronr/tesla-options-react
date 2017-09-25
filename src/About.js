46a728d143a0e96e1c4ed898a0d14271caf58609
Cameron Ring <cameron@cs.stanford.edu>
2017-09-25 00:41:52 +0000

    add about page content

--- src/About.js
+++ src/About.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => (
-  <p>about</p>
-
+  <div className="container">
+    <div className="row">
+      <div className="heading">
+        <h1>Tesla Model S/X Options Decoder</h1>
+        <p className="lead">All rights belong to Tesla Motors. Option codes and descriptions pulled directly from their web site.</p>
+      </div>
+      <div className="col-md-6 col-md-offset-3">
+        <p>
+          I came across Fredrik Fjeld&apos;s original options decoder
+          at <a href="http://options.teslastuff.net/">http://options.teslastuff.net/</a>. After making some
+          improvements, I decided I wanted to reimplment the project in React, both to learn React
+          some more and also so I could add more features to the decoder. So that&apos;s the
+          story :)
+        </p>
+      </div>
+    </div>
+  </div>
 );
 
 export default About;
-
-
